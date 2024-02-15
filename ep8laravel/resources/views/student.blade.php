53ebeec0adf43f74e943b978e4509b3256ffbb81
Your NameMd.Mehadi <mdmehadihassan523@gmail.com>
2024-02-15 09:09:31 +0600

    2laravel02

---
+++ ep8laravel/resources/views/student.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <center>
+            <h1>I am form student view using single action Controller</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita quibusdam modi numquam assumenda eos praesentium, repellat officia pariatur. Velit voluptate esse natus dolorem aspernatur laudantium, vel suscipit voluptates dolor laboriosam?</p>
+    </center>
+</body>
+</html>
